30f249216d9469bac785035166ace8cf394f6fb4
fk2019 <fkmuiruri8@gmail.com>
2025-05-07 01:46:46 +0300

    remove my-app dir

--- my-app/src/app/login/page.js
+++
@@ -1,9 +0,0 @@
-'use client';
-import {LoginPage} from '../../components/LoginPage';
-import {Dashboard} from '../../components/Dashboard';
-export default function Login() {
-  return (
-      <LoginPage/>
-      
-  );
-}
--- my-app/src/app/page.js
+++
@@ -1,9 +0,0 @@
-// src/app/page.tsx
-import { Dashboard } from '../components/Dashboard';
-import AuthWrapper from '../components/AuthWrapper';
-import { LoginPage} from '../components/LoginPage';
-export default function Home() {
-  return
-
-    <Dashboard/>
-}
